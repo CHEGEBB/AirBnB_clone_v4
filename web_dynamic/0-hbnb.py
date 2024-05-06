b871a8b8c3c67ce76b90c38b19d337323b43bec3
CHEGEBB <chegephil24@gmail.com>
2024-05-06 09:54:38 +0300

    first task

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,27 +1,46 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
-from models import storage
-from models.state import State
-from models.city import City
+
+""" 
+This script starts a Flask web application
+It does the following:
+- starts a Flask web application
+- listens on 
+- has a route / that displays "Hello HBNB!"
+- has a route /hbnb that displays "HBNB"
+- has a route /c/<text> that displays "C " followed by the value of the text variable
+"""
+
 from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
 import uuid
+from models import storage
+from models.state import State
+from models.city import City
 app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
 def close_db(error):
-    """ Remove the current SQLAlchemy Session """
+    """
+    This function closes the database or file storage
+    It is triggered when the application context is popped
+    When the application stops, the application context is popped
+    """
     storage.close()
 
 
 @app.route('/0-hbnb/', strict_slashes=False)
 def hbnb():
-    """ HBNB is alive! """
+    """
+    This function renders a template
+    It returns the template at web_dynamic/0-hbnb.html
+    It passes the following variables to the template:
+    - states: a list of State objects
+    - amenities: a list of Amenity objects
+    - places: a list of Place objects
+    """
     states = storage.all(State).values()
     states = sorted(states, key=lambda k: k.name)
     st_ct = []
@@ -43,5 +62,8 @@ def hbnb():
 
 
 if __name__ == "__main__":
-    """ Main Function """
+    """
+    This block of code is executed if this script is run
+    It starts a Flask web application
+    """
     app.run(host='0.0.0.0', port=5001)
